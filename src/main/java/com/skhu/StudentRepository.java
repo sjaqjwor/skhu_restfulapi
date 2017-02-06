374ca348d4eedf12a25ff5258ccb318646cc97a0
sjaqjwor <lsklsk45@naver.com>
2017-02-06 20:38:33 +0900

    no message

--- src/main/java/com/skhu/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skhu;
-
-/**
- * Created by seungki on 2017-02-04.
- */
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
